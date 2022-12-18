0cbe317983ff343ba08daef0f8de05381d0fe460
alexrmn <roman.alex.93@gmail.com>
2022-12-18 11:44:34 +0200

    Updated base entity with JPA annotations. closes #24

--- src/main/java/guru/springframework/petclinic/data/model/BaseEntity.java
+++ src/main/java/guru/springframework/petclinic/data/model/BaseEntity.java
@@ -1,7 +1,15 @@
 package guru.springframework.petclinic.data.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class BaseEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
