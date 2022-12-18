872fad863f51d3fbfe50a88e539c3b209cead83d
alexrmn <roman.alex.93@gmail.com>
2022-12-18 12:23:46 +0200

    updated owner with jpa entity mappings. closes #25

--- src/main/java/guru/springframework/petclinic/data/model/Person.java
+++ src/main/java/guru/springframework/petclinic/data/model/Person.java
@@ -1,9 +1,14 @@
 package guru.springframework.petclinic.data.model;
 
-public class Person extends  BaseEntity{
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
 
-
+@MappedSuperclass
+public class Person extends BaseEntity{
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {
