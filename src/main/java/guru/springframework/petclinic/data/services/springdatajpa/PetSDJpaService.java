aa271deeb5be7e656c8794b0d3b589c0db8ad540
alexrmn <roman.alex.93@gmail.com>
2022-12-19 11:12:40 +0200

    added spring data jpa service for pets. closes #32

--- src/main/java/guru/springframework/petclinic/data/services/springdatajpa/PetSDJpaService.java
+++ src/main/java/guru/springframework/petclinic/data/services/springdatajpa/PetSDJpaService.java
@@ -1,51 +1,48 @@
 package guru.springframework.petclinic.data.services.springdatajpa;
 
-import guru.springframework.petclinic.data.model.PetType;
-import guru.springframework.petclinic.data.model.Vet;
-import guru.springframework.petclinic.data.services.PetTypeService;
+import guru.springframework.petclinic.data.model.Pet;
+import guru.springframework.petclinic.data.services.PetService;
 import guru.springframework.repositories.PetRepository;
-import guru.springframework.repositories.PetTypeRepository;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
-@Service
 @Profile("springdatajpa")
-public class PetSDJpaService implements PetTypeService {
+@Service
+public class PetSDJpaService implements PetService {
 
-    private final PetTypeRepository petTypeRepository;
+    private final PetRepository petRepository;
 
-    public PetSDJpaService(PetTypeRepository petTypeRepository) {
-        this.petTypeRepository = petTypeRepository;
-
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
     }
 
     @Override
-    public Set<PetType> findAll() {
-        Set<PetType> petTypes = new HashSet<>();
-        petTypeRepository.findAll().forEach(petTypes::add);
-        return petTypes;
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
     }
 
     @Override
-    public PetType findById(Long id) {
-        return petTypeRepository.findById(id).orElse(null);
+    public Pet findById(Long id) {
+        return petRepository.findById(id).orElse(null);
     }
 
     @Override
-    public PetType save(PetType petType) {
-        return petTypeRepository.save(petType);
+    public Pet save(Pet pet) {
+        return petRepository.save(pet);
     }
 
     @Override
-    public void delete(PetType petType) {
-        petTypeRepository.delete(petType);
+    public void delete(Pet pet) {
+        petRepository.delete(pet);
     }
 
     @Override
     public void deleteById(Long id) {
-        petTypeRepository.deleteById(id);
+        petRepository.deleteById(id);
     }
 }
